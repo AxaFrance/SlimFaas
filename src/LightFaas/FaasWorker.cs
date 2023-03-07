f2b59f9b49e6327cfb77a91f7cdcce92a1dd6397
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-03-07 14:27:35 +0100

    fix(faaslight): try catch in worker

--- src/LightFaas/FaasWorker.cs
+++ src/LightFaas/FaasWorker.cs
@@ -25,37 +25,52 @@ public class FaasWorker : BackgroundService
     {
         while (stoppingToken.IsCancellationRequested == false)
         {
-            await Task.Delay(10);
-            foreach (var queueKey in _queue.Keys)
+            try
+            {
+                await Task.Delay(10);
+                foreach (var queueKey in _queue.Keys)
+                {
+                    using var scope = _serviceProvider.CreateScope();
+                    var faasLogger = scope.ServiceProvider.GetRequiredService<ILogger<FaasWorker>>();
+                    if (_processingTasks.ContainsKey(queueKey.Key) == false)
+                    {
+                        _processingTasks.Add(queueKey.Key, new List<RequestToWait>());
+                    }
+
+                    var httpResponseMessagesToDelete = new List<RequestToWait>();
+                    foreach (var processing in _processingTasks[queueKey.Key])
+                    {
+                        if (!processing.Task.IsCompleted) continue;
+                        var httpResponseMessage = processing.Task.Result;
+                        faasLogger.LogInformation(
+                            $"{processing.CustomRequest.Method}: {processing.CustomRequest.Path}{processing.CustomRequest.Query} {httpResponseMessage.StatusCode}");
+                        httpResponseMessagesToDelete.Add(processing);
+                    }
+
+                    foreach (var httpResponseMessage in httpResponseMessagesToDelete)
+                    {
+                        _processingTasks[queueKey.Key].Remove(httpResponseMessage);
+                    }
+
+                    if (_processingTasks[queueKey.Key].Count >= queueKey.NumberParallel) continue;
+
+                    var data = _queue.DequeueAsync(queueKey.Key);
+                    if (string.IsNullOrEmpty(data)) continue;
+                    var customRequest = JsonSerializer.Deserialize<CustomRequest>(data);
+                    faasLogger.LogInformation(
+                        $"{customRequest.Method}: {customRequest.Path}{customRequest.Query} Sending");
+                    var taskResponse = scope.ServiceProvider.GetRequiredService<SendClient>()
+                        .SendHttpRequestAsync(customRequest);
+                    _processingTasks[queueKey.Key].Add(new RequestToWait()
+                        { Task = taskResponse, CustomRequest = customRequest });
+                }
+            }
+            catch (Exception e)
             {
                 using var scope = _serviceProvider.CreateScope();
                 var faasLogger = scope.ServiceProvider.GetRequiredService<ILogger<FaasWorker>>();
-                 if(_processingTasks.ContainsKey(queueKey.Key) == false)
-                 {
-                     _processingTasks.Add(queueKey.Key, new List<RequestToWait>());
-                 }
-                 var httpResponseMessagesToDelete = new List<RequestToWait>();
-                 foreach (var processing in _processingTasks[queueKey.Key])
-                 {
-                     if (!processing.Task.IsCompleted) continue;
-                     var httpResponseMessage = processing.Task.Result;
-                     faasLogger.LogInformation($"{processing.CustomRequest.Method}: {processing.CustomRequest.Path}{processing.CustomRequest.Query} {httpResponseMessage.StatusCode}");
-                     httpResponseMessagesToDelete.Add(processing);
-                 }
-
-                 foreach (var httpResponseMessage in httpResponseMessagesToDelete)
-                 {
-                     _processingTasks[queueKey.Key].Remove(httpResponseMessage);
-                 }
-                 
-                 if(_processingTasks[queueKey.Key].Count >= queueKey.NumberParallel) continue;
-                
-                 var data = _queue.DequeueAsync(queueKey.Key);
-                 if (string.IsNullOrEmpty(data)) continue;
-                 var customRequest = JsonSerializer.Deserialize<CustomRequest>(data);
-                 faasLogger.LogInformation($"{customRequest.Method}: {customRequest.Path}{customRequest.Query} Sending");
-                 var taskResponse = scope.ServiceProvider.GetRequiredService<SendClient>().SendHttpRequestAsync(customRequest);
-                 _processingTasks[queueKey.Key].Add(new RequestToWait(){ Task = taskResponse, CustomRequest = customRequest});
+                faasLogger.LogError("Error in FaasWorker: " + e.Message + " " + e.StackTrace);
+                throw;
             }
         }
     }
