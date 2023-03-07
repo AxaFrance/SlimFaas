8d25645acf8b4e281b0fe63ac79d76970d0c3e1b
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-03-07 14:54:26 +0100

    fix(faaslight): add lock to the queue

--- src/LightFaas/Queue.cs
+++ src/LightFaas/Queue.cs
@@ -28,24 +28,35 @@ public class Queue : IQueue
     {
         if(_keys.Count(k => k.Key == key) <= 0)
         {
-            _keys.Add(new QueueKey()
+            lock (this)
             {
+                _keys.Add(new QueueKey()
+                {
+                    Key = key,
+                    NumberParallel = 1,
+                });
+            }
+        }
+
+        lock (this)
+        {
+            messages.Add(new Message()
+            {
+                Data = data,
                 Key = key,
-                NumberParallel = 1,
             });
         }
-        messages.Add(new Message()
-        {
-            Data = data,
-            Key = key,
-        });
     }
         
     public string? DequeueAsync(string key)
     {
         var data = messages.FirstOrDefault(m => m.Key == key);
         if (data == null) return null;
-        messages.Remove(data);
+        lock (this)
+        {
+            messages.Remove(data);
+        }
+
         return data.Data;
     }
 
