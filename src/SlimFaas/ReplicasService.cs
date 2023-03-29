42e0ef98141bed22ff421d3603715ca2db7760b5
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-03-29 18:27:11 +0200

    fix scale

--- src/SlimFaas/ReplicasService.cs
+++ src/SlimFaas/ReplicasService.cs
@@ -2,104 +2,80 @@
 
 public class ReplicasService
 {
-    private readonly IKubernetesService _kubernetesService;
     private readonly HistoryHttpService _historyHttpService;
-    private readonly IServiceProvider _serviceProvider;
-    private  IList<DeploymentInformation> _functions;
+    private readonly IKubernetesService _kubernetesService;
 
-    public ReplicasService(IKubernetesService kubernetesService, HistoryHttpService historyHttpService, IServiceProvider serviceProvider)
+    public ReplicasService(IKubernetesService kubernetesService, HistoryHttpService historyHttpService)
     {
         _kubernetesService = kubernetesService;
         _historyHttpService = historyHttpService;
-        _serviceProvider = serviceProvider;
-        _functions = new List<DeploymentInformation>();
+        Functions = new List<DeploymentInformation>();
     }
 
-    public IList<DeploymentInformation> Functions
-    {
-        get => _functions;
-    }
-    
+    public IList<DeploymentInformation> Functions { get; private set; }
+
     public async Task SyncFunctionsAsync(string kubeNamespace)
     {
         var functions = await _kubernetesService.ListFunctionsAsync(kubeNamespace);
         lock (this)
         {
-            _functions = functions;
+            Functions = functions;
         }
     }
-    
+
     public Task CheckScaleAsync(string kubeNamespace)
     {
         var maximumTicks = 0L;
         IDictionary<string, long> ticksLastCall = new Dictionary<string, long>();
-        foreach (var deploymentInformation in _functions)
+        foreach (var deploymentInformation in Functions)
         {
             var tickLastCall = _historyHttpService.GetTicksLastCall(deploymentInformation.Deployment);
             ticksLastCall.Add(deploymentInformation.Deployment, tickLastCall);
             maximumTicks = Math.Max(maximumTicks, tickLastCall);
         }
 
-        foreach (var deploymentInformation in _functions)
+        var tasks = new List<Task>();
+        foreach (var deploymentInformation in Functions)
         {
-            var tickLastCall = deploymentInformation.ReplicasStartAsSoonAsOneFunctionRetrieveARequest ? maximumTicks : ticksLastCall[deploymentInformation.Deployment];
-            
-            var timeElapsedWhithoutRequest = TimeSpan.FromTicks(tickLastCall) + TimeSpan.FromSeconds(deploymentInformation.TimeoutSecondBeforeSetReplicasMin) <
-                    TimeSpan.FromTicks(DateTime.Now.Ticks);
+            var tickLastCall = deploymentInformation.ReplicasStartAsSoonAsOneFunctionRetrieveARequest
+                ? maximumTicks
+                : ticksLastCall[deploymentInformation.Deployment];
+
+            var timeElapsedWhithoutRequest = TimeSpan.FromTicks(tickLastCall) +
+                                             TimeSpan.FromSeconds(deploymentInformation
+                                                 .TimeoutSecondBeforeSetReplicasMin) <
+                                             TimeSpan.FromTicks(DateTime.Now.Ticks);
             var currentScale = deploymentInformation.Replicas;
+
             if (timeElapsedWhithoutRequest)
             {
                 if (currentScale.HasValue && currentScale > deploymentInformation.ReplicasMin)
                 {
-                    // Fire and Forget
-                    Task.Run(async () =>
+                    var task = _kubernetesService.ScaleAsync(new ReplicaRequest
                     {
-                        var scope = _serviceProvider.CreateScope();
-                        var scopeServiceProvider = scope.ServiceProvider;
-                        var logger = scopeServiceProvider.GetService<ILogger<ReplicasService>>();
-                        try
-                        {
-                            var kubernetesService = scopeServiceProvider.GetService<IKubernetesService>();
-                            await kubernetesService.ScaleAsync(new ReplicaRequest()
-                            {
-                                Replicas = deploymentInformation.ReplicasMin,
-                                Deployment = deploymentInformation.Deployment,
-                                Namespace = kubeNamespace
-                            });
-                        }catch(Exception e)
-                        {
-                            logger.LogError(e, "Error while scaling Down");
-                        }
+                        Replicas = deploymentInformation.ReplicasMin,
+                        Deployment = deploymentInformation.Deployment,
+                        Namespace = kubeNamespace
                     });
 
+                    tasks.Append(task);
                 }
             }
             else if (currentScale is 0)
             {
                 // Fire and Forget
-                Task.Run(async () =>
+                var task = _kubernetesService.ScaleAsync(new ReplicaRequest
                 {
-                    var scope = _serviceProvider.CreateScope();
-                    var scopeServiceProvider = scope.ServiceProvider;
-                    var kubernetesService = scopeServiceProvider.GetService<IKubernetesService>();
-                    var logger = scopeServiceProvider.GetService<ILogger<ReplicasService>>();
-                    try
-                    {
-                        await kubernetesService.ScaleAsync(new ReplicaRequest()
-                        {
-                            Replicas = deploymentInformation.ReplicasAtStart,
-                            Deployment = deploymentInformation.Deployment, Namespace = kubeNamespace
-                        });
-                    }catch(Exception e)
-                    {
-                        logger.LogError(e, "Error while scaling Up");
-                    }
+                    Replicas = deploymentInformation.ReplicasAtStart,
+                    Deployment = deploymentInformation.Deployment, Namespace = kubeNamespace
                 });
 
+                tasks.Append(task);
             }
         }
 
+        Task.WaitAll(tasks.ToArray());
+
         return Task.CompletedTask;
     }
-
 }
