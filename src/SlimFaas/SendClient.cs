4493d4aa2cf2f0535fc7ca3cc9154e25dcf6c642
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-06-12 10:25:16 +0200

    refactor(slimfaas): sync mode stream the response

--- src/SlimFaas/SendClient.cs
+++ src/SlimFaas/SendClient.cs
@@ -3,6 +3,8 @@
 public interface ISendClient
 {
     Task<HttpResponseMessage> SendHttpRequestAsync(CustomRequest customRequest, HttpContext? context = null);
+
+    Task<HttpResponseMessage>  SendHttpRequestSync(HttpContext httpContext, string functionName, string functionPath, string functionQuery);
 }
 
 public class SendClient : ISendClient
@@ -64,9 +66,11 @@ public class SendClient : ISendClient
     public async Task<HttpResponseMessage> SendHttpRequestAsync(CustomRequest customRequest, HttpContext? context = null)
     {
         var functionUrl = _baseFunctionUrl;
-        var url = functionUrl.Replace("{function_name}", customRequest.FunctionName) + customRequest.Path +
-                  customRequest.Query;
-        var targetRequestMessage = CreateTargetMessage(customRequest, new Uri(url));
+        var customRequestFunctionName = customRequest.FunctionName;
+        var customRequestPath = customRequest.Path;
+        var customRequestQuery = customRequest.Query;
+        var targetUrl = ComputeTargetUrl(functionUrl, customRequestFunctionName, customRequestPath, customRequestQuery);
+        var targetRequestMessage = CreateTargetMessage(customRequest, new Uri(targetUrl));
         if (context != null)
         {
             return await _httpClient.SendAsync(targetRequestMessage,
@@ -75,5 +79,53 @@ public class SendClient : ISendClient
         }
         return await _httpClient.SendAsync(targetRequestMessage,
             HttpCompletionOption.ResponseHeadersRead);
+    }
+
+    private static string ComputeTargetUrl(string functionUrl, string customRequestFunctionName, string customRequestPath,
+        string customRequestQuery)
+    {
+        var url = functionUrl.Replace("{function_name}", customRequestFunctionName) + customRequestPath +
+                  customRequestQuery;
+        return url;
+    }
+
+    public async Task<HttpResponseMessage> SendHttpRequestSync(HttpContext context, string functionName, string functionPath, string functionQuery)
+    {
+        var targetUri = ComputeTargetUrl(_baseFunctionUrl, functionName, functionPath, functionQuery);
+        var targetRequestMessage = CreateTargetMessage(context, new Uri(targetUri));
+        using var responseMessage = await _httpClient.SendAsync(targetRequestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted);
+        return responseMessage;
+
+    }
+
+    private HttpRequestMessage CreateTargetMessage(HttpContext context, Uri targetUri)
+    {
+        var requestMessage = new HttpRequestMessage();
+        CopyFromOriginalRequestContentAndHeaders(context, requestMessage);
+
+        requestMessage.RequestUri = targetUri;
+        requestMessage.Headers.Host = targetUri.Host;
+        requestMessage.Method = GetMethod(context.Request.Method);
+
+        return requestMessage;
+    }
+    
+    private void CopyFromOriginalRequestContentAndHeaders(HttpContext context, HttpRequestMessage requestMessage)
+    {
+        var requestMethod = context.Request.Method;
+
+        if (!HttpMethods.IsGet(requestMethod) &&
+            !HttpMethods.IsHead(requestMethod) &&
+            !HttpMethods.IsDelete(requestMethod) &&
+            !HttpMethods.IsTrace(requestMethod))
+        {
+            var streamContent = new StreamContent(context.Request.Body);
+            requestMessage.Content = streamContent;
+        }
+
+        foreach (var header in context.Request.Headers)
+        {
+            requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+        }
     }
 }
