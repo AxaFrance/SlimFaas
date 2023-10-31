9760d70fe1511fece872ac2a3fdb51154246f3f5
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-10-31 15:55:15 +0100

    update

--- src/SlimData/Program.cs
+++ src/SlimData/Program.cs
@@ -1,11 +1,5 @@
-﻿using System.Net;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using DotNext.Net.Cluster.Consensus.Raft;
-using DotNext.Net.Cluster.Consensus.Raft.Http;
-using DotNext.Net.Cluster.Consensus.Raft.Membership;
+﻿using DotNext.Net.Cluster.Consensus.Raft.Http;
 using RaftNode;
-using SslOptions = DotNext.Net.Security.SslOptions;
 
 switch (args.LongLength)
 {
@@ -26,7 +20,7 @@ static Task UseAspNetCoreHost(int port, string? persistentStorage = null)
     var configuration = new Dictionary<string, string>
             {
                 {"partitioning", "false"},
-                {"lowerElectionTimeout", "5000" },
+                {"lowerElectionTimeout", "500" },
                 {"upperElectionTimeout", "6000" },
                 {"requestTimeout", "00:10:00"},
                 {"publicEndPoint", $"http://localhost:{port}"},
@@ -51,84 +45,9 @@ static Task UseAspNetCoreHost(int port, string? persistentStorage = null)
     .RunAsync();
 }
 
-static async Task UseConfiguration(RaftCluster.NodeConfiguration config, string? persistentStorage)
-{
-    AddMembersToCluster(config.UseInMemoryConfigurationStorage());
-    var loggerFactory = LoggerFactory.Create(ConfigureLogging);
-    config.LoggerFactory = loggerFactory;
-    using var cluster = new RaftCluster(config);
-    cluster.LeaderChanged += ClusterConfigurator.LeaderChanged;
-    var modifier = default(DataModifier?);
-    if (!string.IsNullOrEmpty(persistentStorage))
-    {
-        var state = new SimplePersistentState(persistentStorage);
-        cluster.AuditTrail = state;
-        modifier = new DataModifier(cluster, state);
-    }
-    await cluster.StartAsync(CancellationToken.None);
-    await (modifier?.StartAsync(CancellationToken.None) ?? Task.CompletedTask);
-    using var handler = new CancelKeyPressHandler();
-    Console.CancelKeyPress += handler.Handler;
-    await handler.WaitAsync();
-    Console.CancelKeyPress -= handler.Handler;
-    await (modifier?.StopAsync(CancellationToken.None) ?? Task.CompletedTask);
-    await cluster.StopAsync(CancellationToken.None);
-
-    // NOTE: this way of adding members to the cluster is not recommended in production code
-    static void AddMembersToCluster(InMemoryClusterConfigurationStorage<EndPoint> storage)
-    {
-        var builder = storage.CreateActiveConfigurationBuilder();
-
-        builder.Add(new IPEndPoint(IPAddress.Loopback, 3262));
-        builder.Add(new IPEndPoint(IPAddress.Loopback, 3263));
-        builder.Add(new IPEndPoint(IPAddress.Loopback, 3264));
-
-        builder.Build();
-    }
-}
-
 static void ConfigureLogging(ILoggingBuilder builder)
     => builder.AddConsole().SetMinimumLevel(LogLevel.Error);
 
-static Task UseUdpTransport(int port, string? persistentStorage)
-{
-    var configuration = new RaftCluster.UdpConfiguration(new IPEndPoint(IPAddress.Loopback, port))
-    {
-        LowerElectionTimeout = 150,
-        UpperElectionTimeout = 300,
-        DatagramSize = 1024,
-        ColdStart = false,
-    };
-    return UseConfiguration(configuration, persistentStorage);
-}
-
-static Task UseTcpTransport(int port, string? persistentStorage, bool useSsl)
-{
-    var configuration = new RaftCluster.TcpConfiguration(new IPEndPoint(IPAddress.Loopback, port))
-    {
-        RequestTimeout = TimeSpan.FromMilliseconds(1000),
-        LowerElectionTimeout = 50,
-        UpperElectionTimeout = 500,
-        TransmissionBlockSize = 4096,
-        ColdStart = true,
-        SslOptions = useSsl ? CreateSslOptions() : null
-    };
-
-    return UseConfiguration(configuration, persistentStorage);
-
-    static SslOptions CreateSslOptions() => new()
-    {
-        ServerOptions = new()
-        {
-            ServerCertificate = LoadCertificate()
-        },
-        ClientOptions = new()
-        {
-            TargetHost = "localhost",
-            RemoteCertificateValidationCallback = RaftClientHandlerFactory.AllowCertificate
-        }
-    };
-}
 
 static Task StartNode(string protocol, int port, string? persistentStorage = null)
 {
@@ -137,24 +56,9 @@ static Task StartNode(string protocol, int port, string? persistentStorage = nul
         case "http":
         case "https":
             return UseAspNetCoreHost(port, persistentStorage);
-        case "udp":
-            return UseUdpTransport(port, persistentStorage);
-        case "tcp":
-            return UseTcpTransport(port, persistentStorage, false);
-        case "tcp+ssl":
-            return UseTcpTransport(port, persistentStorage, true);
         default:
             Console.Error.WriteLine("Unsupported protocol type");
             Environment.ExitCode = 1;
             return Task.CompletedTask;
     }
-}
-
-static X509Certificate2 LoadCertificate()
-{
-    using var rawCertificate = Assembly.GetCallingAssembly().GetManifestResourceStream(typeof(Program), "node.pfx");
-    using var ms = new MemoryStream(1024);
-    rawCertificate?.CopyTo(ms);
-    ms.Seek(0, SeekOrigin.Begin);
-    return new X509Certificate2(ms.ToArray(), "1234");
 }
