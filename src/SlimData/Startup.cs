48255a94311a359a41086ea0f9220533161aeb1b
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-12-08 15:12:10 +0100

    clean up endpoints

--- src/SlimData/Startup.cs
+++ src/SlimData/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using DotNext;
+﻿using DotNext;
 using DotNext.Net.Cluster.Consensus.Raft;
 using DotNext.Net.Cluster.Consensus.Raft.Http;
 using Microsoft.AspNetCore.Connections;
@@ -10,14 +9,7 @@ namespace RaftNode;
 public class Startup(IConfiguration configuration)
 {
     public static readonly IList<string> ClusterMembers = new List<string>(2);
-
-    private static Task RedirectToLeaderAsync(HttpContext context)
-    {
-        var cluster = context.RequestServices.GetRequiredService<IRaftCluster>();
-        return context.Response.WriteAsync(
-            $"Leader address is {cluster.Leader?.EndPoint}. Current address is {context.Connection.LocalIpAddress}:{context.Connection.LocalPort}",
-            context.RequestAborted);
-    }
+    
 
     public void Configure(IApplicationBuilder app)
     {
@@ -27,6 +19,7 @@ public class Startup(IConfiguration configuration)
         const string ListLeftPushResource = "/SlimData/ListLeftPush";
         const string AddKeyValueResource = "/SlimData/AddKeyValue";
         const string ListLengthResource = "/SlimData/ListLength";
+        const string HealthResource = "/SlimData/ListLength";
 
         app.UseConsensusProtocolHandler()
             .RedirectToLeader(LeaderResource)
@@ -38,232 +31,15 @@ public class Startup(IConfiguration configuration)
             .UseRouting()
             .UseEndpoints(static endpoints =>
             {
-                endpoints.MapGet(LeaderResource, RedirectToLeaderAsync);
-                endpoints.MapGet("/SlimData/health", async context => { await context.Response.WriteAsync("OK"); });
-                endpoints.MapPost(ListLeftPushResource, async context =>
-                {
-                    var slimDataInfo = context.RequestServices.GetRequiredService<SlimDataInfo>();
-                    if (context.Request.Host.Port != slimDataInfo.Port)
-                    {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                        return;
-                    }
-
-                    var cluster = context.RequestServices.GetRequiredService<IRaftCluster>();
-                    var provider = context.RequestServices.GetRequiredService<SlimPersistentState>();
-                    var source =
-                        CancellationTokenSource.CreateLinkedTokenSource(context.RequestAborted,
-                            cluster.LeadershipToken);
-                    try
-                    {
-                        var form = await context.Request.ReadFormAsync(source.Token);
-
-                        var key = string.Empty;
-                        var value = string.Empty;
-
-                        foreach (var keyValue in form)
-                        {
-                            key = keyValue.Key;
-                            value = keyValue.Value.ToString();
-                            break;
-                        }
-
-                        if (string.IsNullOrEmpty(key))
-                        {
-                            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                            await context.Response.WriteAsync("not data found", context.RequestAborted);
-                            return;
-                        }
-
-                        var logEntry =
-                            provider.interpreter.CreateLogEntry(new ListLeftPushCommand { Key = key, Value = value },
-                                cluster.Term);
-                        await provider.AppendAsync(logEntry, source.Token);
-                        await provider.CommitAsync(source.Token);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Unexpected error {0}", e);
-                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                    }
-                    finally
-                    {
-                        source?.Dispose();
-                    }
-                });
-
-                endpoints.MapPost(ListRightPopResource, async context =>
-                {
-                    var slimDataInfo = context.RequestServices.GetRequiredService<SlimDataInfo>();
-                    if (context.Request.Host.Port != slimDataInfo.Port)
-                    {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                        return;
-                    }
-
-                    var cluster = context.RequestServices.GetRequiredService<IRaftCluster>();
-                    var provider = context.RequestServices.GetRequiredService<SlimPersistentState>();
-                    var source =
-                        CancellationTokenSource.CreateLinkedTokenSource(context.RequestAborted,
-                            cluster.LeadershipToken);
-                    try
-                    {
-                        var form = await context.Request.ReadFormAsync(source.Token);
-
-                        var key = string.Empty;
-                        var value = string.Empty;
-                        foreach (var formData in form)
-                        {
-                            key = formData.Key;
-                            value = formData.Value.ToString();
-                            break;
-                        }
-
-                        if (string.IsNullOrEmpty(key) || !int.TryParse(value, out var count))
-                        {
-                            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                            await context.Response.WriteAsync("Key key is empty or value is not a number",
-                                context.RequestAborted);
-                            return;
-                        }
-
-                        await cluster.ApplyReadBarrierAsync(context.RequestAborted);
-
-                        var values = new ListString();
-                        var queues = ((ISupplier<SupplierPayload>)provider).Invoke().Queues;
-                        if (queues.ContainsKey(key))
-                        {
-                            var queue = ((ISupplier<SupplierPayload>)provider).Invoke().Queues[key];
-                            for (var i = 0; i < count; i++)
-                            {
-                                if (queue.Count <= i) break;
-
-                                values.Add(queue[i]);
-                            }
-
-                            await context.Response.WriteAsync(
-                                JsonSerializer.Serialize(values, ListStringSerializerContext.Default.ListString),
-                                context.RequestAborted);
-                            var logEntry =
-                                provider.interpreter.CreateLogEntry(
-                                    new ListRightPopCommand { Key = key, Count = count },
-                                    cluster.Term);
-                            await provider.AppendAsync(logEntry, source.Token);
-                            await provider.CommitAsync(source.Token);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Unexpected error {0}", e);
-                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                    }
-                    finally
-                    {
-                        source?.Dispose();
-                    }
-                });
-                endpoints.MapPost(AddHashSetResource, async context =>
-                {
-                    var slimDataInfo = context.RequestServices.GetRequiredService<SlimDataInfo>();
-                    if (context.Request.Host.Port != slimDataInfo.Port)
-                    {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                        return;
-                    }
-
-                    var cluster = context.RequestServices.GetRequiredService<IRaftCluster>();
-                    var provider = context.RequestServices.GetRequiredService<SlimPersistentState>();
-                    var source =
-                        CancellationTokenSource.CreateLinkedTokenSource(context.RequestAborted,
-                            cluster.LeadershipToken);
-                    try
-                    {
-                        var form = await context.Request.ReadFormAsync(source.Token);
-
-                        var key = string.Empty;
-                        var dictionary = new Dictionary<string, string>();
-                        foreach (var formData in form)
-                            if (formData.Key == "______key_____")
-                                key = formData.Value.ToString();
-                            else
-                                dictionary[formData.Key] = formData.Value.ToString();
-
-                        if (string.IsNullOrEmpty(key))
-                        {
-                            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                            await context.Response.WriteAsync("Key ______key_____ is empty", context.RequestAborted);
-                            return;
-                        }
-
-                        var logEntry =
-                            provider.interpreter.CreateLogEntry(
-                                new AddHashSetCommand { Key = key, Value = dictionary }, cluster.Term);
-                        await provider.AppendAsync(logEntry, source.Token);
-                        await provider.CommitAsync(source.Token);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Unexpected error {0}", e);
-                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                    }
-                    finally
-                    {
-                        source?.Dispose();
-                    }
-                });
-                endpoints.MapPost(AddKeyValueResource, async context =>
-                {
-                    var slimDataInfo = context.RequestServices.GetRequiredService<SlimDataInfo>();
-                    if (context.Request.Host.Port != slimDataInfo.Port)
-                    {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                        return;
-                    }
-
-                    var cluster = context.RequestServices.GetRequiredService<IRaftCluster>();
-                    var provider = context.RequestServices.GetRequiredService<SlimPersistentState>();
-                    var source =
-                        CancellationTokenSource.CreateLinkedTokenSource(context.RequestAborted,
-                            cluster.LeadershipToken);
-                    try
-                    {
-                        var form = await context.Request.ReadFormAsync(source.Token);
-
-                        var key = string.Empty;
-                        var value = string.Empty;
-
-                        foreach (var keyValue in form)
-                        {
-                            key = keyValue.Key;
-                            value = keyValue.Value.ToString();
-                            break;
-                        }
-
-                        if (string.IsNullOrEmpty(key))
-                        {
-                            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                            await context.Response.WriteAsync("not data found", context.RequestAborted);
-                            return;
-                        }
-
-                        var logEntry =
-                            provider.interpreter.CreateLogEntry(new AddKeyValueCommand { Key = key, Value = value },
-                                cluster.Term);
-                        await provider.AppendAsync(logEntry, source.Token);
-                        await provider.CommitAsync(source.Token);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Unexpected error {0}", e);
-                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                    }
-                    finally
-                    {
-                        source?.Dispose();
-                    }
-                });
+                endpoints.MapGet(LeaderResource, Endpoints.RedirectToLeaderAsync);s
+                endpoints.MapGet(HealthResource, async context => { await context.Response.WriteAsync("OK"); });
+                endpoints.MapPost(ListLeftPushResource,  Endpoints.ListLeftPush);
+                endpoints.MapPost(ListRightPopResource,  Endpoints.ListRigthPop);
+                endpoints.MapPost(AddHashSetResource,  Endpoints.AddHashSet);
+                endpoints.MapPost(AddKeyValueResource,  Endpoints.AddKeyValue);
             });
     }
+
 
     public void ConfigureServices(IServiceCollection services)
     {
