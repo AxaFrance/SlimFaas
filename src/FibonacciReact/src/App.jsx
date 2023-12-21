2c44a14f5493de62d27fa6b9feaa4471dcecb3c0
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-12-21 21:30:23 +0100

    update

--- src/FibonacciReact/src/App.jsx
+++ src/FibonacciReact/src/App.jsx
@@ -23,16 +23,25 @@ function useInterval(callback, delay) {
 
 function App({ name, url }) {
   const [state, setState] = useState( {"status": "not_started"});
+  const [stateInterval, setInterval] = useState(true);
+  const [stateSeconds, setSeconds] = useState(0);
   const [stateFibonacci, setFibonacci] = useState( {});
   useInterval(() => {
-    fetch( url + '/status-function/'+name).then((res) => res.json()).then((data) => {
-        data.NumberReady = data.NumberReady || 0;
-        data.NumberRequested = data.NumberRequested || 0;
-        let status = data.NumberReady === data.NumberRequested ? "ready" : "loading";
-        if(data.NumberRequested === 0) status = "not_started";
-        setState({status: status});
-    });
-  }, 2000);
+      if(stateInterval) {
+          setInterval(false);
+          fetch(url + '/status-function/' + name).then((res) => res.json()).then((data) => {
+              data.NumberReady = data.NumberReady || 0;
+              data.NumberRequested = data.NumberRequested || 0;
+              let status = data.NumberReady === data.NumberRequested ? "ready" : "loading";
+              if (data.NumberRequested === 0) status = "not_started";
+              setState({status: status});
+          }).then(() => {
+            setInterval(true);
+          });
+      }
+  }, 1000);
+
+    useInterval(() => { setSeconds(stateSeconds+1) }, 1000);
 
   const postFibonacciAsync = () => {
     const start = performance.now();
@@ -66,7 +75,8 @@ function App({ name, url }) {
                 Wake up
             </button>
             <p>
-               Environement status: {state.status} <br/>
+                Seconds: {stateSeconds} <br/>
+               Environment status: {state.status} <br/>
                Request status: {JSON.stringify(stateFibonacci)}
             </p>
         </div>
