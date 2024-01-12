58676919d2e4a0f64bad2ddd9be9a18711d8210a
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-01-12 19:35:45 +0100

    update

--- src/SlimData/RaftClientHandlerFactory.cs
+++ src/SlimData/RaftClientHandlerFactory.cs
@@ -7,10 +7,10 @@ internal sealed class RaftClientHandlerFactory : IHttpMessageHandlerFactory
 {
     public HttpMessageHandler CreateHandler(string name)
     {
-        var slimDataUpperElectionTimeout = Environment.GetEnvironmentVariable("SLIMDATA_UPPER_ELECTION_TIMEOUT") ?? "600";
+        var slimDataUpperElectionTimeout = Environment.GetEnvironmentVariable("SLIMDATA_REQUEST_TIMEOUT") ?? "600";
         if (!int.TryParse(slimDataUpperElectionTimeout, out int upperElectionTimeout))
         {
-            throw new Exception("SLIMDATA_UPPER_ELECTION_TIMEOUT is not an integer");
+            throw new Exception("SLIMDATA_REQUEST_TIMEOUT is not an integer");
         }
         var handler = new SocketsHttpHandler { ConnectTimeout = TimeSpan.FromMilliseconds(upperElectionTimeout) };
         handler.SslOptions.RemoteCertificateValidationCallback = AllowCertificate;
