db4bd09334f7016642a0e6207448d02ad166074f
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-01-19 17:09:35 +0100

    fix(slimdata): relection

--- src/SlimData/RaftClientHandlerFactory.cs
+++ src/SlimData/RaftClientHandlerFactory.cs
@@ -8,15 +8,26 @@ internal sealed class RaftClientHandlerFactory : IHttpMessageHandlerFactory
 {
     public HttpMessageHandler CreateHandler(string name)
     {
-        var slimDataUpperElectionTimeout = Environment.GetEnvironmentVariable(EnvironmentVariables.SlimDataSocketsHttpHandlerTimeout) ?? EnvironmentVariables.SlimDataSocketsHttpHandlerTimeoutDefault;
-        if (!int.TryParse(slimDataUpperElectionTimeout, out int upperElectionTimeout))
+        Console.WriteLine($"RaftClientHandlerFactory.CreateHandler({name})");
+        if (name != "RaftClient")
         {
-            throw new Exception("SLIMDATA_SOCKETS_HTTP_HANDLER_TIMEOUT is not an integer");
-        }
-        var handler = new SocketsHttpHandler { ConnectTimeout = TimeSpan.FromMilliseconds(upperElectionTimeout)};
-        handler.SslOptions.RemoteCertificateValidationCallback = AllowCertificate;
-        handler.UseProxy = false;
-        return handler;
+            var slimDataSocketsHttpHandlerTimeoutDefault =
+                Environment.GetEnvironmentVariable(EnvironmentVariables.SlimDataSocketsHttpHandlerTimeout) ??
+                EnvironmentVariables.SlimDataSocketsHttpHandlerTimeoutDefault;
+            if (!int.TryParse(slimDataSocketsHttpHandlerTimeoutDefault, out int upperElectionTimeout))
+            {
+                throw new Exception("SLIMDATA_SOCKETS_HTTP_HANDLER_TIMEOUT is not an integer");
+            }
+
+            var handler = new SocketsHttpHandler { ConnectTimeout = TimeSpan.FromMilliseconds(upperElectionTimeout) };
+            handler.SslOptions.RemoteCertificateValidationCallback = AllowCertificate;
+            handler.UseProxy = false;
+            return handler;
+        } 
+        var handlerDefault = new SocketsHttpHandler();
+        handlerDefault.SslOptions.RemoteCertificateValidationCallback = AllowCertificate;
+        handlerDefault.UseProxy = false;
+        return handlerDefault;
     }
 
     internal static bool AllowCertificate(object sender, X509Certificate? certificate, X509Chain? chain,
