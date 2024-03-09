9602d808a4b7a6860d6746389e8790ec989b2fd9
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-03-09 09:15:44 +0100

    fix(slimfaas): pods desynchronization

--- src/SlimFaas/ReplicasSynchronizationWorker.cs
+++ src/SlimFaas/ReplicasSynchronizationWorker.cs
@@ -23,7 +23,7 @@ public class ReplicasSynchronizationWorker(IReplicasService replicasService,
         {
             try
             {
-                if(cluster is { Leader: not null, LeadershipToken.IsCancellationRequested: true } )
+                /*if(cluster is { Leader: not null, LeadershipToken.IsCancellationRequested: true } )
                 {
                     await Task.Delay(_delay/10, stoppingToken);
                     var currentDeploymentsJson = await slimDataService.GetAsync(kubernetesDeployments);
@@ -40,9 +40,11 @@ public class ReplicasSynchronizationWorker(IReplicasService replicasService,
                 }
                 else
                 {
-                    await Task.Delay(_delay, stoppingToken);
-                    var deployments = await replicasService.SyncDeploymentsAsync(_namespace);
-                    if (cluster.Leader == null)
+                    */
+                await Task.Delay(_delay, stoppingToken);
+                   /* var deployments = */
+                await replicasService.SyncDeploymentsAsync(_namespace);
+                /*    if (cluster.Leader == null)
                     {
                         continue;
                     }
@@ -52,7 +54,7 @@ public class ReplicasSynchronizationWorker(IReplicasService replicasService,
                     {
                         await slimDataService.SetAsync(kubernetesDeployments, newDeploymentsJson);
                     }
-                }
+                }*/
             }
             catch (Exception e)
             {
