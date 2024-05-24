fd5f3c6ae7424fd39093045fc9d662817ce49cb0
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-05-24 15:54:28 +0200

    feature(all): merge swagger

--- src/Fibonacci/Program.cs
+++ src/Fibonacci/Program.cs
@@ -5,14 +5,18 @@ IServiceCollection serviceCollection = builder.Services;
 serviceCollection.AddSingleton<Fibonacci, Fibonacci>();
 serviceCollection.AddCors();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 WebApplication app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors(builder => builder
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader()
 );
-
-
 
 app.MapPost("/fibonacci", (
     [FromServices] ILogger<Fibonacci> logger,
@@ -24,22 +28,34 @@ app.MapPost("/fibonacci", (
     output.Result = fibonacci.Run(input.Input);
     return output;
 });
+/*
+app.MapPost("/crazy-fibonacci", (
+    [FromServices] ILogger<Fibonacci> logger,
+    [FromServices] Fibonacci fibonacci,
+    FibonacciInput input) =>
+{
+    logger.LogDebug("Fibonacci Called");
+    var output = new FibonacciOutput();
+    output.Result = fibonacci.Run(input.Input);
+    return output;
+});*/
+
+// http://localhost:30021/function/fibonacci1/hello/guillaume
 
 app.MapGet("/download", ([FromServices] ILogger<Fibonacci> logger) =>
 {
     logger.LogDebug("Download Called");
     string path = Path.Combine(Directory.GetCurrentDirectory(), "dog.png");
     return Results.File(path, "image/png");
-});
+}).WithDescription("Some Method Description");
 
 app.MapGet("/hello/{name}", ([FromServices] ILogger<Fibonacci> logger, string name) =>
 {
     logger.LogDebug("Hello Called");
     return $"Hello {name}!";
-});
+}).WithDescription("Some Method Description");
 
-
-app.MapGet("/health", () => "OK");
+app.MapGet("/health", () => "OK").WithDescription("Some Method Description");
 
 app.Run();
 
@@ -55,6 +71,33 @@ internal class Fibonacci
         return Run(i - 1) + Run(i - 2);
     }
 }
+/*
+internal class CrazyFibonacci
+{
+    private readonly HttpClient _httpClient;
+
+    public CrazyFibonacci(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<int> Run(int i)
+    {
+        if (i <= 2)
+        {
+            return 1;
+        }
+
+        var random =new Random();
+        var value = random.Next(1, 3);
+        var response = await _httpClient.PostAsJsonAsync($"http://localhost:30021/function/fibonacci{value}", new FibonacciInput(){Input = i});
+
+        var content = await response.Content.ReadAsStringAsync();
+
+
+        return Run(i - 1) + Run(i - 2);
+    }
+}*/
 
 public record FibonacciInput{
     public int Input { get; set; }
