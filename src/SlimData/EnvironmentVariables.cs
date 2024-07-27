78f6adbd372a63c164959a980b64689c6f7465c9
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-07-27 21:58:25 +0200

    fix(slimdata): Update EnvironmentVariables.cs

--- src/SlimData/EnvironmentVariables.cs
+++ src/SlimData/EnvironmentVariables.cs
@@ -3,5 +3,5 @@
 public class EnvironmentVariables
 {
     public const string SlimDataSocketsHttpHandlerTimeout = "SLIMDATA_SOCKETS_HTTP_HANDLER_TIMEOUT";
-    public const string SlimDataSocketsHttpHandlerTimeoutDefault = "400";
+    public const string SlimDataSocketsHttpHandlerTimeoutDefault = "100";
 }
