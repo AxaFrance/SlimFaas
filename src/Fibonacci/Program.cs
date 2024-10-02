2bcbf4e369f619f7e52a0aa2f278663403698fe9
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-10-02 16:30:56 +0200

    fix

--- src/Fibonacci/Program.cs
+++ src/Fibonacci/Program.cs
@@ -51,26 +51,46 @@ app.MapPost("/fibonacci-recursive", async (
     [FromServices] HttpClient client,
     FibonacciInput input) =>
 {
-    logger.LogInformation("Fibonacci Recursive Internal Called: {Input}", input.Input);
-    var output = new FibonacciOutput();
-    if (input.Input <= 2)
+    try
     {
-        output.Result = 1;
+        logger.LogInformation("Fibonacci Recursive Internal Called: {Input}", input.Input);
+        var output = new FibonacciOutput();
+        if (input.Input <= 2)
+        {
+            output.Result = 1;
+            return output;
+        }
+
+        var response1 =
+            client.PostAsJsonAsync(
+                "http://slimfaas.slimfaas-demo.svc.cluster.local:5000/function/fibonacci1/fibonacci-recursive",
+                new FibonacciInput() { Input = input.Input - 1 });
+        var response2 =
+            client.PostAsJsonAsync(
+                "http://slimfaas.slimfaas-demo.svc.cluster.local:5000/function/fibonacci1/fibonacci-recursive",
+                new FibonacciInput() { Input = input.Input - 2 });
+        if (response1.Result.IsSuccessStatusCode || response2.Result.IsSuccessStatusCode)
+        {
+            throw new Exception("Request Fail");
+        }
+
+        var result1 =
+            JsonConvert.DeserializeObject<FibonacciOutput>(await response1.Result.Content.ReadAsStringAsync());
+        logger.LogInformation("Current result1: {Result}", result1.Result);
+        var result2 =
+            JsonConvert.DeserializeObject<FibonacciOutput>(await response2.Result.Content.ReadAsStringAsync());
+        logger.LogInformation("Current result2: {Result}", result2.Result);
+        output.Result = result1.Result + result2.Result;
+        logger.LogInformation("Current output: {Result}", output.Result);
         return output;
     }
-    var response1 = client.PostAsJsonAsync("http://slimfaas.slimfaas-demo.svc.cluster.local:5000/function/fibonacci1/fibonacci-recursive", new FibonacciInput(){ Input = input.Input-1});
-    var response2 = client.PostAsJsonAsync("http://slimfaas.slimfaas-demo.svc.cluster.local:5000/function/fibonacci1/fibonacci-recursive", new FibonacciInput(){ Input = input.Input-2});
-    if (response1.Result.IsSuccessStatusCode || response2.Result.IsSuccessStatusCode)
+    catch (Exception ex)
     {
-        throw new Exception("Request Fail");
+        logger.LogError(ex, "Error in Fibonacci Recursive Internal");
     }
-    var result1 = JsonConvert.DeserializeObject<FibonacciOutput>(await response1.Result.Content.ReadAsStringAsync());
-    logger.LogInformation("Current result1: {Result}", result1.Result);
-    var result2 = JsonConvert.DeserializeObject<FibonacciOutput>(await response2.Result.Content.ReadAsStringAsync());
-    logger.LogInformation("Current result2: {Result}", result2.Result);
-    output.Result = result1.Result + result2.Result;
-    logger.LogInformation("Current output: {Result}", output.Result);
-    return output;
+
+
+    return new FibonacciOutput();
 });
 
 
