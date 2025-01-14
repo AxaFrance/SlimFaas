eaed9fb5298d8bdf7fd3bdbf62255ac1dae7a9b3
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-01-14 09:34:19 +0100

    fix(slimfaas): metrics deployment name does not match regex '^[a-zA-Z_][a-zA-Z0-9_]*$

--- src/SlimFaas/MetricsWorker.cs
+++ src/SlimFaas/MetricsWorker.cs
@@ -25,19 +25,20 @@ public class MetricsWorker(IReplicasService replicasService, ISlimFaasQueue slim
                 var deployments = replicasService.Deployments;
                 foreach (var deployment in deployments.Functions)
                 {
+                    var deploymentName = deployment.Deployment.Replace("-", "_").ToLowerInvariant();
                     var numberElementAvailable = await slimFaasQueue.CountElementAsync(deployment.Deployment, new List<CountType>() { CountType.Available });
                     dynamicGaugeService.SetGaugeValue(
-                        $"slimfaas_queue_available_{deployment.Deployment.ToLowerInvariant()}_length",
+                        $"slimfaas_queue_available_{deploymentName}_length",
                         numberElementAvailable, "Current number of elements available in the queue");
 
                     var numberElementProcessing = await slimFaasQueue.CountElementAsync(deployment.Deployment, new List<CountType>() { CountType.Running });
                     dynamicGaugeService.SetGaugeValue(
-                        $"slimfaas_queue_processing_{deployment.Deployment.ToLowerInvariant()}_length",
+                        $"slimfaas_queue_processing_{deploymentName}_length",
                         numberElementProcessing, "Current number of elements processing in the queue");
 
                     var numberElementWaitingForRetry = await slimFaasQueue.CountElementAsync(deployment.Deployment, new List<CountType>() { CountType.WaitingForRetry });
                     dynamicGaugeService.SetGaugeValue(
-                        $"slimfaas_queue_waiting_for_retry_{deployment.Deployment.ToLowerInvariant()}_length",
+                        $"slimfaas_queue_waiting_for_retry_{deploymentName}_length",
                         numberElementWaitingForRetry, "Current number of elements waiting for retry in the queue");
                 }
             }
