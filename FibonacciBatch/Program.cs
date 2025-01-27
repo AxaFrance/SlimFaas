afbb5ae52fc3f53db5b1d9c9d2b9a634df484026
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-01-27 22:01:24 +0100

    add fibonacci batch for demo

---
+++ FibonacciBatch/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+foreach(string arg in args){
+    int i = int.Parse(arg);
+    var fibonacci = new Fibonacci();
+    Console.WriteLine(fibonacci.Run(i));
+}
+
+internal class Fibonacci
+{
+    public int Run(int i)
+    {
+        if (i <= 2)
+        {
+            return 1;
+        }
+
+        return Run(i - 1) + Run(i - 2);
+    }
+}
